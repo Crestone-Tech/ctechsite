0c9c28e5f62954949fec7b6fd7a13e6ddce84d48
cuhrmunken <elliott.davis@crestonetech.com>
2024-10-15 14:22:15 -0700

    adds banner skeleton

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,9 @@
+// import React from "react";
+import "./banner.css";
+export default function Banner() {
+  return (
+    <>
+      <div className="banner">Header Banner with Logo</div>;
+    </>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
+import Banner from "../Banner";
 import Nav from "../Nav";
 import "./header.css";
 export default function Header() {
   return (
-    <header>
-      <div>
-        <a href="#">
-          <img src="/home_icon.jpg" alt="Crestone Technology icon" />
-        </a>
-      </div>
-      <Nav />
-    </header>
+    <>
+      <header>
+        <div>
+          <a href="#">
+            <img src="/home_icon.jpg" alt="Crestone Technology icon" />
+          </a>
+        </div>
+        <Nav />
+      </header>
+      <Banner />
+    </>
   );
 }
