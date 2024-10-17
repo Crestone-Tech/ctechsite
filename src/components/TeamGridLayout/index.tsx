d3ba8498e7869fdb362a895b257f974efcb6d174
cuhrmunken <elliott.davis@crestonetech.com>
2024-10-17 16:52:27 -0700

    adds membersData

--- src/components/TeamGridLayout/index.tsx
+++ src/components/TeamGridLayout/index.tsx
@@ -13,15 +13,50 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-const members = [
-  { memberName: "Violet" },
-  { memberName: "Johnny" },
-  { memberName: "Gabby" },
-  { memberName: "Adam" },
-  { memberName: "Cody" },
-  { memberName: "Jeff" },
-  { memberName: "Megan" },
-  { memberName: "Elliott" },
+type MemberInfo = {
+  name: string;
+  github: string;
+  linkedIn: string;
+};
+
+// TODO move this type to an index.d.ts file? @Cody?
+const membersData: MemberInfo[] = [
+  {
+    name: "Violet",
+    github: "https://github.com/floweringvi",
+    linkedIn: "https://www.linkedin.com/in/violet-stanziani-a82196297/",
+  },
+  {
+    name: "Johnny",
+    github: "",
+    linkedIn: "https://www.linkedin.com/in/jonathan-baldyga-bb666b321/",
+  },
+  {
+    name: "Gabby",
+    github: "",
+    linkedIn: "https://www.linkedin.com/in/gcbelanger/",
+  },
+  {
+    name: "Adam",
+    github: "",
+    linkedIn: "https://www.linkedin.com/in/adammathis05/",
+  },
+  {
+    name: "Cody",
+    github: "",
+    linkedIn: "https://www.linkedin.com/in/steven-thaller/",
+  },
+  { name: "Jeff", github: "", linkedIn: "" },
+  {
+    name: "Megan",
+    github: "",
+    linkedIn: "https://www.linkedin.com/in/megan-mcneill/",
+  },
+  {
+    name: "Elliott",
+    github: "https://github.com/ec-davis",
+    linkedIn: "https://www.linkedin.com/in/ec-davis/",
+  },
 ];
 
 export default function Team() {
@@ -29,14 +64,18 @@ export default function Team() {
     <>
       <SectionTitle sectionTitle="Team" />
       <Grid container spacing={2}>
-        {members.map((member, index) => (
+        {membersData.map((member, index) => (
           <Grid size={{ xs: 12, sm: 6, md: 4 }} key={index}>
             <Card>
               <CardContent>
-                <Typography variant="h5">{member.memberName}</Typography>
+                <Typography variant="h5">{member.name}</Typography>
               </CardContent>
               <Tooltip title="GitHub Profile">
-                <IconButton>
+                <IconButton
+                  onClick={() => {
+                    window.open(member.github);
+                  }}
+                >
                   <GitHubIcon />
                 </IconButton>
               </Tooltip>
@@ -46,7 +85,11 @@ export default function Team() {
                 </IconButton>
               </Tooltip>
               <Tooltip title="LinkedIn Profile">
-                <IconButton>
+                <IconButton
+                  onClick={() => {
+                    window.open(member.linkedIn);
+                  }}
+                >
                   <LinkedInIcon />
                 </IconButton>
               </Tooltip>
