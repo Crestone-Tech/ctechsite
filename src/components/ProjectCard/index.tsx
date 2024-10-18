97c9a6d2a278b8001b11da3ae1fd9598bd48c7a3
cuhrmunken <elliott.davis@crestonetech.com>
2024-10-18 06:21:12 -0700

    adds project image in a clumsy fashion, WIP

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,17 +1,27 @@
+import Grid from "@mui/material/Grid2";
 import "./project-card.css";
-
+import { Card, CardContent, Typography } from "@mui/material";
 export default function ProjectCard() {
   return (
     <>
-      <div className="project-card-row">
-        <div className="project-card-image">Project Image </div>
-        <div className="project-card-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
-          sint in nisi. Iure nulla error dolore consectetur mollitia aspernatur.
-          Consequatur, ducimus. Consectetur explicabo nesciunt temporibus!
-          Consequuntur nostrum debitis aperiam natus!
-        </div>
-      </div>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, sm: 6 }}>
+          <Card>
+            <CardContent>
+              <img
+                className="project-card-image"
+                src="/greentrail-project.jpg"
+                alt="Greentrail application screen"
+              />
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent>
+              <Typography>Project Name</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 }
