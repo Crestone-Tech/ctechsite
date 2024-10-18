fcaed134ea5ff4303d9737596ef491b140382312
cuhrmunken <elliott.davis@crestonetech.com>
2024-10-18 05:45:16 -0700

    removes divider code

--- src/components/HorizontalDivider/index.tsx
+++
@@ -1,9 +0,0 @@
-//import React from "react";
-
-export default function HorizontalDivider() {
-  return (
-    <div className="sectionTitle">
-      -----------------------------------------
-    </div>
-  );
-}
