271c04033d870f77a92e9a51c196bfc43722b7cc
cuhrmunken <elliott.davis@crestonetech.com>
2024-10-21 15:58:59 -0700

    add TODO

--- src/components/TeamMemberCard/index.tsx
+++ src/components/TeamMemberCard/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface TeamMemberCardProps {
   memberName: string;
 }
-
+// TODO replace with new type
 const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ memberName }) => {
   return <div className="team-member-card">{memberName}</div>;
 };
