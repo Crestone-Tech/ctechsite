8864ca49eac570971500903265c8e0cc88dc0ae7
Megan McNeill <mcneim2@gmail.com>
2024-10-20 21:58:12 -0400

    added logo to public, basic html for footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,17 @@ import "./footer.css";
 export default function Footer() {
   return (
     <>
-      <footer>Footer</footer>
+      <footer>
+        {/* TODO: add crestone logo */}
+        <div>
+          <img src="/logoStroked.png" />
+        </div>
+        {/* TODO: add github logo that links to repo */}
+        <div>
+        <p>Designed & Built by Crestone Technology</p>
+        <p>Copyright 2024</p>
+        </div>
+        </footer>
     </>
   );
 }
