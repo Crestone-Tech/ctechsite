797d904879a06eb4490f0a930d566c4e042ecb85
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-10-22 14:03:56 -0400

    Added Header image and merged hero with navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
 import Nav from "../Nav";
+import mainLogo from "../../assets/images/mainLogo.jpg";
+import "../Nav/nav.css";
 import "./header.css";
 export default function Header() {
   return (
     <>
       <header>
-        <div>
-          <a href="#">
+        <div className="logo-container">
+          <img
+            src={mainLogo}
+            alt="Crestone Technology logo"
+            className="main-logo"
+          />
+          <a href="/" className="home-icon">
             <img src="/home_icon.jpg" alt="Crestone Technology icon" />
           </a>
+          <div className="navbar-overlay">
+            <Nav />
+          </div>
         </div>
-        <Nav />
       </header>
     </>
   );
