4f2a8e339c8ab9766cec013cb8bc215cf12ed25c
Megan McNeill <mcneim2@gmail.com>
2024-10-21 22:32:22 -0400

    styled footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,19 @@
 import "./footer.css";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Footer() {
   return (
     <>
       <footer>
-        {/* TODO: add crestone logo */}
         <div>
           <img src="/logoStroked.png" />
         </div>
-        {/* TODO: add github logo that links to repo */}
+        {/* TODO: add link to repo to Icon */}
         <div>
-        <p>Designed & Built by Crestone Technology</p>
-        <p>Copyright 2024</p>
+          <p>Designed & Built by Crestone Technology <GitHubIcon /> </p>
+          <p>Copyright 2024</p>
         </div>
-        </footer>
+      </footer>
     </>
   );
 }
