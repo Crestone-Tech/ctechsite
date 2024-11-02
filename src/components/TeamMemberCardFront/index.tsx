f67443a08eebb251982664006b2420ca29cd8b3e
Megan McNeill <mcneim2@gmail.com>
2024-11-02 16:59:49 -0400

    added avatar types, added avatar file locations to membersData, changed team-member-card-front extention to .css, added avatar as a prop in teammembercardfront, started styling team-member-card

--- src/components/TeamMemberCardFront/index.tsx
+++ src/components/TeamMemberCardFront/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import {
   Card,
   CardContent,
-  Typography
+  Typography,
+  Avatar
 } from "@mui/material";
 
 // Import the MemberInfo type
@@ -11,18 +12,22 @@ import { MemberInfo } from "../../types/MemberInfo";
 // import TeamLinkBar component
 import TeamLinkBar from "../TeamLinkBar";
 
+// import the css
+import "./team-member-card-front.css"
+
 interface TeamMemberCardFrontProps {
   memberInfo: MemberInfo;
 }
 // TODO replace with new type
 const TeamMemberCardFront: React.FC<TeamMemberCardFrontProps> = ({ memberInfo }) => {
-  const { name } = memberInfo;
+  const { name, avatar } = memberInfo;
 
   return (
     <div className="team-member-card">
-      <Card>
-        <CardContent>
+      <Card id="cardFront">
+        <CardContent >
           <Typography variant="h5">{name}</Typography>
+          <Avatar alt={name} src={avatar} sx={{ width: 120, height: 120 }}/>
         </CardContent>
         <TeamLinkBar memberInfo={memberInfo}/>
       </Card>
