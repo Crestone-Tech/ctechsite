5e03365a74d8c0d360726a14fe136f53af64cbab
cuhrmunken <elliott.davis@crestonetech.com>
2024-11-12 09:44:33 -0700

    shrinks the image, adds ComponentTitle,

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,17 +1,33 @@
 import Grid from "@mui/material/Grid2";
 import "./project-card.css";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, CardHeader} from "@mui/material";
 export default function ProjectCard() {
   return (
     <>
       <Grid size={{ xs: 12, sm: 6 }}>
         <Card>
-          <CardContent>
+          <CardHeader
+            title="Nifty Project"
+            sx={{
+              textAlign: "center",
+              padding: "0 1rem .5rem",
+            }}
+          ></CardHeader>
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              flexGrow: 1, // Allow this section to grow
+              p: 0,
+            }}
+          >
+            
             <img
-              className="project-card-image"
-              srcSet={`/greentrail-project.jpg?w=248&fit=crop&auto=format&dpr=2 4x`}
+              srcSet={`/greentrail-project.jpg?w=248&dpr=2 4x`}
               // ELLIOTT: I found this code, that uses both srcSet and src, in the MUI website. I do not fully understand it.
-              src={`/greentrail-project.jpg?w=248&fit=crop&auto=format`}
+              src={`/greentrail-project.jpg`}
               loading="lazy"
               alt="Greentrail application screen"
             />
