613ae9b79f3d151b13ec7ab6699522ec04c1a6f8
cuhrmunken <elliott.davis@crestonetech.com>
2024-11-14 09:39:31 -0700

    added second project to data file

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -9,4 +9,11 @@ export const projectsData: ProjectInfo[] = [
       "is a project like a lorem, and it ipsums the dolor sit ametizizing the bing-bog!, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     screenshot: "/team_images/Fox.png",
   },
+  {
+    name: "Gym COP",
+    github: "https://www.wikihow.com/Make-Slime",
+    description:
+      "is a project like a ipsums; it bibppity-bip-bipsums. Twice on Tuesdays, it goes out to fix-sums. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    screenshot: "/team_images/Frog.png",
+  },
 ];
