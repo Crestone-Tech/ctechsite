f0a208031ff7a55c769c93dfc0bff9c80b705cf0
cuhrmunken <elliott.davis@crestonetech.com>
2024-11-18 16:33:28 -0700

    opens simple dialog

--- src/components/ContactDialog/index.tsx
+++ src/components/ContactDialog/index.tsx
@@ -1,24 +1,48 @@
 import React from "react";
 import { Dialog, DialogTitle, Button } from "@mui/material";
 
+export interface SimpleDialogProps {
+  open: boolean;
+  selectedValue: string;
+  onClose: (value: string) => void;
+}
+
+function SimpleDialog(props: SimpleDialogProps) {
+  const handleClose = () => {
+    props.onClose(props.selectedValue);
+  };
+
+  return (
+    <>
+      <Dialog onClose={props.onClose} open={props.open}>
+        <DialogTitle>Foprksalot</DialogTitle>
+      </Dialog>
+    </>
+  );
+}
+
 export default function ContactDialog() {
   const [open, setOpen] = React.useState(false);
-  
-//   toggling dialog show/hide
+
+  //   toggling dialog show/hide
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
-  }
+  };
 
   return (
     <>
       <Button variant="outlined" onClick={handleClickOpen}>
         Open simple dialog
       </Button>
-      <p>{open}</p>
+      <SimpleDialog
+        open={open}
+        onClose={handleClose}
+        selectedValue="flog"
+      ></SimpleDialog>
     </>
   );
 }
