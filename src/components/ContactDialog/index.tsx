6914dac8332d4e9721a1f1ef807a301b40043807
cuhrmunken <elliott.davis@crestonetech.com>
2024-11-19 08:58:20 -0700

    good enough layout in ContactDlg

--- src/components/ContactDialog/index.tsx
+++ src/components/ContactDialog/index.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { Dialog, DialogTitle, Button } from "@mui/material";
+import ContactDlg from "../ContactDlg";
+// export interface SimpleDialogProps {
+//   open: boolean;
+//   selectedValue: string;
+//   onClose: (value: string) => void;
+// }
 
-export interface SimpleDialogProps {
-  open: boolean;
-  selectedValue: string;
-  onClose: (value: string) => void;
-}
+// function SimpleDialog(props: SimpleDialogProps) {
+//   const handleClose = () => {
+//     props.onClose(props.selectedValue);
+//   };
 
-function SimpleDialog(props: SimpleDialogProps) {
-  const handleClose = () => {
-    props.onClose(props.selectedValue);
-  };
-
-  return (
-    <>
-      <Dialog onClose={props.onClose} open={props.open}>
-        <DialogTitle>Foprksalot</DialogTitle>
-      </Dialog>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Dialog onClose={props.onClose} open={props.open}>
+//         <DialogTitle>Foprksalot</DialogTitle>
+//       </Dialog>
+//     </>
+//   );
+// }
 
 export default function ContactDialog() {
   const [open, setOpen] = React.useState(false);
@@ -36,13 +36,13 @@ export default function ContactDialog() {
   return (
     <>
       <Button variant="outlined" onClick={handleClickOpen}>
-        Open simple dialog
+        Contact Us
       </Button>
-      <SimpleDialog
+      <ContactDlg
         open={open}
         onClose={handleClose}
         selectedValue="flog"
-      ></SimpleDialog>
+      ></ContactDlg>
     </>
   );
 }
---
+++ src/components/ContactDlg/index.tsx
@@ -0,0 +1,82 @@
+import { Dialog, DialogActions, DialogTitle, Button } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+
+export interface ContactDlgProps {
+  open: boolean;
+  selectedValue: string;
+  onClose: (value: string) => void;
+}
+
+export default function SimpleDialog(props: ContactDlgProps) {
+  const handleClose = () => {
+    props.onClose(props.selectedValue);
+  };
+
+  return (
+    <>
+      <Dialog onClose={props.onClose} open={props.open}>
+        <DialogTitle>We would love to hear from you</DialogTitle>
+        <Grid container spacing={2} padding={4}>
+          <Grid container spacing={3} padding={2}>
+            <Grid>
+              <TextField
+                id="first-name"
+                label="First Name"
+                required
+                variant="standard"
+                fullWidth
+              />
+            </Grid>
+            <Grid>
+              <TextField
+                id="last-name"
+                label="Last Name"
+                required
+                variant="standard"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <Grid padding={2}>
+            <TextField
+              id="email"
+              label="Email"
+              required
+              fullWidth
+              variant="standard"
+              aria-describedby="email-address-helper-text"
+            />
+            <FormHelperText id="email-address-helper-text">
+              We'll never share your email address
+            </FormHelperText>
+
+            <TextField
+              id="email-subject"
+              label="Subject"
+              variant="standard"
+              fullWidth
+            />
+
+            <TextField
+              id="message"
+              label=""
+              required
+              multiline
+              rows={3}
+              fullWidth
+              variant="standard"
+              placeholder="Message"
+              sx={{ marginTop: 2 }} // Add spacing above
+            />
+          </Grid>
+        </Grid>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Send Message</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
