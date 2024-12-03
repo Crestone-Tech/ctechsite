0f991e20cf32075531a5f70cc75c1c6cd5bac3fe
cuhrmunken <elliott.davis@crestonetech.com>
2024-12-03 10:44:50 -0700

    adds working Hello, world! server skeleton

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
