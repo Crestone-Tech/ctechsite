90b2ea9e2bbee0d9fdad340e29744be54fbc699c
JohnnyBaldyga <jonathanbaldyga@gmail.com>
2024-12-03 11:48:44 -0500

    Adds new about me text and styling to match figma

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,16 +1,20 @@
-
-import SectionTitle from "../SectionTitle"
+import SectionTitle from "../SectionTitle";
 
 export default function About() {
   return (
     <>
-      <SectionTitle sectionTitle="About Us" />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
-        repellendus quaerat nemo amet cum ex, sunt quam ad aut, accusamus
-        commodi illo veniam, voluptas perferendis labore dolorum facilis tenetur
-        officiis!
-      </p>
+      <div className="aboutContainer">
+        <SectionTitle sectionTitle="About Us" />
+        <p>
+          Crestone Technology is a team of developers working on open-source
+          projects together. Our mission is to create innovative and
+          user-friendly digital tools that empower communities and businesses to
+          thrive in the digital age. With a passion for collaboration and a
+          commitment to quality, we focus on building tools that make a
+          difference, contributing to the open-source ecosystem while fostering
+          a culture of learning and growth.
+        </p>
+      </div>
     </>
   );
 }
