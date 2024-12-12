b95851d28b8b4db23f2491c68d0a4b6ff857635b
Megan McNeill <mcneim2@gmail.com>
2024-12-12 11:07:55 -0500

    updated variable names and comments for clarity

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -13,12 +13,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function TeamCarousel() {
-  // setup the state variable and setter for the display index of each slide (display index = data-index of className slide-content), setting initial data-index to 0
-  const [displayIndex, setDisplayIndex] = useState<number>(0);
+  // setup the state variable and setter that tracks the index of the center slide (centerSlideIndex = the index of the slide that is in the center)
+  const [centerSlideIndex, setCenterSlideIndex] = useState<number>(0);
 
   // Function to calculate styles based on the slide-content's data-index (0 being the center -1 -2 etc to the left and 1 2 etc to the right)
-  const indexedStyles = (dataIndex: number) => {
-    const distance = Math.abs(dataIndex); // absolute value of data-index
+  const indexedStyles = (displayIndex: number) => {
+    const distance = Math.abs(displayIndex); // absolute value of data-index
     const scale = distance === 0 ? 1.2 : 1 - 0.0 * distance; // Active slide is scaled up to 1.2, other slides are scalled down in relation to distance
     const opacity = distance === 0 ? 1 : 1 - 0.5 * distance; // Active slide has full opacity, other slides have lower opacity in relation to distance
     const zIndex = distance === 0 ? 5 : 5 - distance; // Active slide has a higher z-index, other slides have lower zIndex in relation to distance
@@ -37,11 +37,12 @@ export default function TeamCarousel() {
     infinite: true,
     centerPadding: "0px", // Space between center and adjacent slides
     slidesToShow: 3, // Number of slides to display at once
-    speed: 100, // Transition speed in ms
+    speed: 10, // Transition speed in ms
     arrows: true, // Enable navigation arrows
-    // afterChange is an event in react slick that detects when the active slide changes, we are using that event to update the displayIndex state
+    swipe:true,
+    // afterChange is an event in react slick that detects when the active slide changes, afterChange receives the active slide's index, we are using that event to update the centerIndex state
     afterChange: (index: number) => {
-      setDisplayIndex(index);
+      setCenterSlideIndex(index);
     },
   };
 
@@ -90,19 +91,21 @@ export default function TeamCarousel() {
         {membersData.map((member, index) => {
           // set a variable for the total number of slides
           const totalSlides = membersData.length;
-          // Each slide gets a data-index attribute to map its index with its style. index is the index of the data in membersData. The dataIndex calcuation account for the wrapping of the infinite carousel (ie the last slide being index 8 and the slide following it being index 0). It calculates 0 at the center position negative whole numbers to the left and postive whoel numbers to the right.
-          const dataIndex =
-            ((((index - displayIndex + Math.floor(totalSlides / 2)) % totalSlides) + totalSlides) % totalSlides) - Math.floor(totalSlides / 2);
+          // Each slide gets a display-index attribute to map its rendered position with its style. index is the index of the data in membersData. The displayIndex calcuation accounts for the wrapping of the infinite carousel (ie the last slide being index 8 and the slide following it being index 0). It calculates to 0 at the center position, negative whole numbers to the left and postive whole numbers to the right.
+          const displayIndex =
+            ((((index - centerSlideIndex + Math.floor(totalSlides / 2)) % totalSlides) + totalSlides) % totalSlides) - Math.floor(totalSlides / 2);
           return (
             <Box
               className="slide-content"
               key={index}
               sx={{
-                ...indexedStyles(dataIndex), // Apply styles based on data-index
+                ...indexedStyles(displayIndex), // Apply styles based on displayIndex
                 position: "relative",
                 margin: "1rem",
               }}
-              data-index={dataIndex} // Set data-index attribute
+              display-index={displayIndex} // Set display-index attribute
+              centerSlide-index={centerSlideIndex}
+              slide-index={index}
             >
               <TeamMemberCardFront
                 // passing member object to TeamMemberCardFront
