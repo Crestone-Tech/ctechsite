f0e0f1fac78579afcb10a973082998c15af9688c
cuhrmunken <elliott.davis@crestonetech.com>
2024-12-19 10:10:06 -0700

    restores email button, copies email address to clipboard

--- src/components/TeamGridLayout/index.tsx
+++ src/components/TeamGridLayout/index.tsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
+// for CT-81, to finish the MVP release, we are 
+// - simplifying, not flipping the cards
+// - preserving the ability to flip the cards by commenting and keeping code (below)
+//   See Story CT-81 
+            // https://crestonetech.atlassian.net/browse/CT-81?atlOrigin=eyJpIjoiYjhmZTk5YjM5NDAwNDhlYjg2MmNhOTczN2NmN2EzNjMiLCJwIjoiaiJ9
+// CT-81 import { useState } from "react";
 import { Box } from "@mui/material";
 import SectionTitle from "../SectionTitle";
 import { membersData } from "../../data/membersData";
 import TeamMemberCardFront from "../TeamMemberCardFront/index";
-import TeamMemberCardBack from "../TeamMemberCardBack/index";
+// CT-81 import TeamMemberCardBack from "../TeamMemberCardBack/index";
 
 export default function TeamGridLayout() {
   // State to track the index of the currently flipped card
-  const [flippedCardIndex, setFlippedCardIndex] = useState<number | null>(null);
+  // CT-81
+  // const [flippedCardIndex, setFlippedCardIndex] = useState<number | null>(null);
 
   // Function to toggle the card flip state
-  const toggleCard = (index: number) => {
-    // If the clicked card is already flipped, set it to null (flip back)
-    // Otherwise, set the index of the clicked card
-    setFlippedCardIndex((prevIndex) => (prevIndex === index ? null : index));
-  };
+  // CT-81
+  // const toggleCard = (index: number) => {
+  //   // If the clicked card is already flipped, set it to null (flip back)
+  //   // Otherwise, set the index of the clicked card
+  //   setFlippedCardIndex((prevIndex) => (prevIndex === index ? null : index));
+  // };
   return (
     // ToDo The px should probably be universally applied to everything on the page except the header and footer
     <Box
@@ -32,17 +39,15 @@ export default function TeamGridLayout() {
       >
         {membersData.map((member, index) => (
           <Box margin="1rem" key={index}>
-            <div onClick={() => toggleCard(index)}>
+            {/* CT-81 <div onClick={() => toggleCard(index)}> */}
+            <div>
               <TeamMemberCardFront
                 // passing member object to TeamMemberCardFront
                 memberInfo={member}
               />
             </div>
 
-            {/* Preserving the ability to flip the cards. For MVP, we are simplifying, not flipping the cards
-
-              See Story CT-81 
-            https://crestonetech.atlassian.net/browse/CT-81?atlOrigin=eyJpIjoiYjhmZTk5YjM5NDAwNDhlYjg2MmNhOTczN2NmN2EzNjMiLCJwIjoiaiJ9
+            {/* See Story CT-81 for why this is commented out
 
             {flippedCardIndex === index ? (
               <div onClick={() => toggleCard(index)}>
